5917192b9635507036b95d54b67c919e791c501a
gayathri-117 <gayathri.s1@care.ac.in>
2025-09-25 12:01:30 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,24 @@
-output "public_ip" {
-  value = aws_instance.app.public_ip
+output "instance_id" {
+  description = "EC2 instance id"
+  value       = aws_instance.app.id
 }
-output "ssh_command" {
-  value = "ssh -i <path-to-pem> ec2-user@${aws_instance.app.public_ip}"
+
+output "instance_public_ip" {
+  description = "EC2 public IP"
+  value       = aws_instance.app.public_ip
+}
+
+output "security_group_id" {
+  description = "Created Security Group ID"
+  value       = aws_security_group.strapi_sg.id
+}
+
+output "iam_role_name" {
+  description = "IAM role name for EC2"
+  value       = aws_iam_role.ec2_role.name
+}
+
+output "image_full" {
+  description = "Image deployed (input)"
+  value       = var.image_full
 }
